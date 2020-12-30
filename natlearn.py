3e0a78bb4f6d513afcf9518b75a6a07e37f49647
Paul Tarau <ptarau@gmail.com>
2020-12-29 22:04:26 -0600

    progress on learning a ground database

--- natlearn.py
+++ natlearn.py
@@ -3,6 +3,7 @@ from natlog.db import *
 from natlog.parser import parse
 #from natlog.scanner import Int
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 from answerer import tsv2mat
 import numpy as np
@@ -36,39 +37,93 @@ def wss2hotXy(wss,io_split = -1) :
   #print(coldX[0])
   return enc,hotX,enc_,hoty
 
-def wss2hotX(wss,io_split = -1) :
+def wss2hotX(wss,mask) :
+  mask=[mask]*len(wss[0])
   X=np.array(wss)
-  print(X.shape)
+  Xplus=np.array(wss+[mask])
+  print(Xplus.shape)
   enc=OneHotEncoder(handle_unknown='ignore')
   enc.fit(X)
   hotX=enc.transform(X).toarray()
+  return enc,X,hotX
 
-  return enc,hotX
+rf_learner=RandomForestClassifier(random_state=1234)
+nn_learner=MLPClassifier(hidden_layer_sizes=(128,64,128))
 
 class natlearner(natlog) :
   def __init__(self,
                text=None,
                file_name=None,
                tsv_file='out/texts/english.tsv',
-               learner=RandomForestClassifier(random_state=1234)
+               learner=nn_learner
                ):
     if not text and not file_name : text = ""
     super().__init__(text=text,file_name=file_name)
+    self.mask='###'
     self.rels=tsv2db(fname=tsv_file)
-    self.enc,self.hotX=wss2hotX(self.rels.css)
+    self.y0=np.array(self.rels.css)
+    self.enc,self.X,self.hotX=wss2hotX(self.rels.css,self.mask)
     self.learner=learner
 
   def train(self):
-    return self.learner.fit(self.hotX,self.hotX)
+    width=self.y0.shape[1]
+
+    yargs=[self.hotX for _ in range(width)]
+    y=np.concatenate(yargs,axis=0)
+
+    maskColumn=np.array([self.mask]*self.X.shape[0])
+
+    xargs=[]
+    for i in range(width) :
+      Xi=self.X.copy()
+      Xi[:,i]=maskColumn
+      xargs.append(Xi)
+
+    X=np.concatenate(tuple(xargs),axis=0)
+    X=self.enc.transform(X)
+
+    print(X.shape,y.shape)
+    return self.learner.fit(X,y)
 
   def ask(self,qss):
     Q=np.array(qss)
-    print('###',Q.shape)
+
+
+    for j in range(10): print('Q', j*10, Q[j*10])
+
+    Q[0, 0] = self.mask
+    Q[0, 3] = self.mask
+    Q[0, 5] = self.mask
+
+    Q[20, 0] = self.mask
+    Q[20, 3] = self.mask
+    Q[20, 5] = self.mask
+
+    #Q[30, 0] = self.mask
+    Q[30, 1] = self.mask
+    Q[30, 2] = self.mask
+    Q[30, 3] = self.mask
+    #Q[30, 4] = self.mask
+    #Q[30, 5] = self.mask
+
+
     hotQ=self.enc.transform(Q).toarray()
 
-    assert hotQ.shape==self.hotX.shape
+    assert hotQ.shape[1]==self.hotX.shape[1]
 
-    # TODO
+    hotA=(self.learner.predict(hotQ))
+
+    print('\nANSWERS',hotA.shape,'\n')
+
+
+    A=self.enc.inverse_transform(hotA)
+
+
+    for j in range(10) : print('A',j*10,A[j*10])
+
+    #print('!!!!',A)
+    return A
+
 
 
 # tests
@@ -104,4 +159,6 @@ if __name__=="__main__" :
   nl=natlearner()
   nl.train()
   nl.ask(nl.rels.css)
+  #qss=[['this','DET','det','NOUN','cat',nl.mask]]
+  #nl.ask(qss)
   pass
