58c0e2d135ee2ff2ee033c4c7916cde76102125d
Yifan-G <yifan.guo.3517@gmail.com>
2021-06-12 11:44:25 -0500

    small change

--- walker.py
+++ walker.py
@@ -135,11 +135,8 @@ if __name__=="__main__":
   print('OR that you give the path of a directory where pdfs/ is a subdirectory')
 
   params=dict(
-    # rootdir = "/Users/tarau/Desktop/sit/GRAPHSTAX/",
-    # pdfs = "biblion/"
-    rootdir = "/Users/tarau/Desktop/sit/MISC/",
-    pdfs="sienna2021/"
-
+    rootdir = "/Users/tarau/Desktop/sit/GRAPHSTAX/",
+    pdfs = "biblion/"
   )
   summarize_all()
   #summarize_all(**params)
