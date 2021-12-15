0cfb14514e45c3daa719903a4abfd3ef11bb9c74
Paul Tarau <ptarau@gmail.com>
2021-12-14 21:43:17 -0600

    added scaler that summarizes folders

---
+++ textstar/scaler.py
@@ -0,0 +1,209 @@
+import os
+import subprocess
+import glob
+import sys
+from multiprocessing import Pool, cpu_count
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+from textstar import *
+
+
+def out_dirs():
+    out = 'out/'
+    ensure_path(out + 'any')
+    fs = [out + x for x in
+          ('overview.txt',
+           'pdftexts/',
+           'sums/',
+           'keys/'
+           )
+          ]
+
+    for f in fs:
+        ensure_path(f)
+    return fs
+
+
+def exists_file(fname):
+    """ if it exists as file or dir"""
+    return os.path.exists(fname)
+
+
+def ensure_path(fname):
+    folder, _ = os.path.split(fname)
+    os.makedirs(folder, exist_ok=True)
+
+
+def pdf2txt(pdf, txt):
+    subprocess.run(["pdftotext", "-q", pdf, txt])
+    if os.path.getsize(txt) > 32:
+        return True
+    os.remove(txt)
+    return False
+
+
+def file2string(fname):
+    with open(fname, 'r') as f:
+        return f.read()
+
+
+def string2file(text, fname):
+    with open(fname, 'w') as g:
+        g.write(text)
+
+
+def clean_text_file(fname, lang=None):
+    # print('cleaning: '+fname)
+    data = file2string(fname)
+    if len(data) < 100:
+        s = [x for x in data if x != ' ']
+        if len(s) < 10:
+            return False
+    if lang is None:
+        lang = detect_lang(data)
+    if lang != 'en': return None
+    texts = sent_tokenize(data)
+    clean = []
+    for text in texts:
+        ws = word_tokenize(text)
+        good = 0
+        bad = 0
+        if len(ws) > 256: continue
+        for w in ws:
+            if w.isalpha() and len(w) > 1:
+                good += 1
+            else:
+                bad += 1
+        if good / (1 + bad + good) < 0.75: continue
+        if ws[-1] not in ".?!": ws.append(".")
+        sent = " ".join(ws)
+        clean.append(sent)
+    new_data = "\n".join(clean)
+    string2file(new_data, fname)
+    return True
+
+
+def walk(wdir="./"):
+    for filename in sorted(set(glob.iglob(wdir + '**/**', recursive=True))):
+        yield filename
+
+
+def summarize_one(pdf, trim, texts, sums, keys, lang):
+    """ summarizer for one document
+    """
+    if pdf[-4:].lower() != ".pdf": return None
+
+    name = pdf[trim:-4]
+
+    tname0 = texts + name
+    tname = texts + name + ".txt"
+    sname = sums + name + ".txt"
+    kname = keys + name + ".txt"
+
+    ensure_path(tname)
+    try:
+        print('START processing:', pdf)
+        if not (pdf2txt(pdf, tname)):
+            print('Unable to convert from PDF, skipping file!')
+            return None
+        clean_text_file(tname, lang=lang)
+
+        sents, kws = summarize(tname0)
+        sents = [sent for (_sid,sent) in sents]
+
+        ktext = "\n".join(kws)
+        ensure_path(kname)
+        string2file(ktext, kname)
+
+        stext = "\n".join(sents)
+        ensure_path(sname)
+        string2file(stext, sname)
+        print('WRITTEN TO', sname, kname)
+
+        text = "\n".join(
+            ['FILE:', pdf, '\nSUMMARY:', stext, '\nKEYWORDS:', ktext, '\n'])
+        print('DONE processing:', pdf)
+        return text
+    except IndexError:
+        print('ERROR:', sys.exc_info()[0])
+        print('Processing failed on:', pdf)
+        return None
+    except ValueError:
+        return None
+    except RuntimeError:
+        return None
+
+
+def summarize_all(
+    rootdir=None,
+    pdfs="../pdfs/",
+    lang='en',
+):
+    """ sequential summarizer"""
+
+    overview, texts, sums, keys = out_dirs()
+
+    if rootdir:
+        rootdir = os.path.abspath(rootdir) + "/"
+        names = (pdfs, overview, texts, sums, keys)
+        pdfs, overview, texts, sums, keys = tuple(rootdir + x for x in names)
+    ensure_path(overview)
+    with open(overview, 'w') as outf:
+        trim = len(pdfs)
+        for pdf in walk(wdir=pdfs):
+            text = summarize_one(pdf, trim, texts, sums, keys, lang)
+            if not text: continue
+            print(text, file=outf)
+            # print(text)
+            # break
+
+
+def sum_one(args):
+    return summarize_one(*args)
+
+
+def parsum_all(rootdir=None, pdfs="pdfs/", lang='en'):
+    """ parallel summarizer"""
+
+    overview, texts, sums, keys = out_dirs()
+
+    if rootdir:
+        rootdir = os.path.abspath(rootdir) + "/"
+        names = (pdfs, overview, texts, sums, keys)
+        pdfs, overview, texts, sums, keys = tuple(rootdir + x for x in names)
+
+    count = max(2, cpu_count() // 3)
+    with Pool(processes=count) as pool:
+        trim = len(pdfs)
+        fs = [pdf for pdf in walk(wdir=pdfs) if pdf[-4:].lower() == ".pdf"]
+        pdf_count = len(fs)
+        chunksize = 1  # max(1,int(l/(4*count)))
+        print('pdf files:', pdf_count, 'processes:', count, 'chunksize:', chunksize)
+        args = [(pdf, trim, texts, sums, keys, lang) for pdf in fs]
+        ensure_path(overview)
+        with open(overview, 'w') as outf:
+            for text in pool.imap(sum_one, args, chunksize=chunksize):
+                if text:
+                    print(text, file=outf)
+
+
+if __name__ == "__main__":
+    print('MAKE SURE you have created  "pdfs/" directory with ".pdf" files in it')
+    print('OR that you give the path of a directory where pdfs/ is a subdirectory')
+
+    params = dict(
+        # rootdir = "/Users/tarau/Desktop/sit/GRAPHSTAX/",
+        # pdfs = "biblion/"
+        # rootdir="/Users/tarau/Desktop/paps/",
+        rootdir="/Users/tarau/Desktop/paps/",
+        pdfs="hyperbolics/"
+        # rootdir = "/Users/tarau/Desktop/sit/MISC/",
+        # pdfs="sienna2021/"
+
+    )
+    #summarize_all()
+    # parsum_all()
+    summarize_all(**params)
+
+    # parsum_all(**params)
--- textstar/textstar.py
+++ textstar/textstar.py
@@ -1,6 +1,8 @@
 # from multiprocessing import Pool, cpu_count
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+
 import networkx as nx
 
 
@@ -18,17 +20,53 @@ def text2sents(text):
     lss = []
     for sent in sents:
         ws = word_tokenize(sent)
+        wts = pos_tag(ws)
+
         ls = []
-        for w in ws:
+        for w, t in wts:
             lw = w.lower()
             if not lw.isalpha(): continue
             if lw in stops: continue
             lemma = lemmatizer.lemmatize(w)
             if lemma in stops: continue
-            ls.append(lemma)
+            tag = t[0]
+            ls.append(W(lemma, w, tag))
         if not ls: continue
         lss.append((ls, sent))
     return lss
+
+
+class W:
+    def __init__(self, lemma, word, tag):
+        self.lemma = lemma
+        self.word = word
+        self.tag = tag
+
+    def __repr__(self):
+        return f"W('{self.lemma}','{self.word}','{self.tag}')"
+
+
+def add_compounds(g, sid, ls):
+    cs = 0
+    m = len(ls)
+    added = set()
+    for i, w in enumerate(ls):
+        if i < m - 2 and w.tag in 'J' and ls[i + 1].tag in 'NJ' and ls[i + 2].tag in 'N':
+            t = " ".join([w.word, ls[i + 1].word, ls[i + 2].word])
+            for x in ls[i:i + 3]:
+                f = x.lemma
+                g.add_edge(f, t)
+                added.add(i)
+                added.add(i + 1)
+                added.add(i + 2)
+                g.add_edge(sid, t)
+        elif i < m - 1 and i not in added and (i + 1) not in added and \
+            w.tag in 'NJ' and ls[i + 1].tag in 'N':
+            for x in ls[i:i + 2]:
+                f = x.lemma
+                t = " ".join([w.word, ls[i + 1].word])
+                g.add_edge(f, t)
+                g.add_edge(sid, t)
 
 
 def sents2graph(lss):
@@ -37,94 +75,95 @@ def sents2graph(lss):
     for sent_id, (ls, _) in enumerate(lss):
         if sent_id > 0:  # from sent to sent before it
             g.add_edge(sent_id, sent_id - 1)
-        g.add_edge(ls[0], sent_id)  # from 1-st word to sent id
-        g.add_edge(sent_id, ls[-1])  # from sent id to last word
+        g.add_edge(ls[0].lemma, sent_id)  # from 1-st word to sent id
+        g.add_edge(sent_id, ls[-1].lemma)  # from sent id to last word
         # g.add_edge(ls[0], ls[-1]) # from first word to last word
         for j, w in enumerate(ls):
-            if j > 0: g.add_edge(w, ls[j - 1])
+            if j > 0: g.add_edge(w.lemma, ls[j - 1].lemma)
+        add_compounds(g, sent_id, ls)
     return g
 
 
-def textstar(g, ranker, sumsize, kwsize, trim, flip):
+def textstar(g, ranker, sumsize, kwsize, trim):
+    assert sumsize > 0 or kwsize > 0
+    final_sids, final_kwds = None, None
     while True:
-        gbak = g.copy()
-
-        print(gbak)
 
         unsorted_ranks = ranker(g)
         ranks = sorted(unsorted_ranks.items(), reverse=True, key=lambda x: x[1])
+
+        sids = [sid for (sid, _) in ranks if isinstance(sid, int)]
+        kwds = [w for (w, _) in ranks if isinstance(w, str)]
+
+        print('=> S_NODES:', len(sids), 'W_NODES', len(kwds))
+
         total = len(ranks)
         split = trim * total // 100
         # print(ranks)
         weak_nodes = [n for (n, _) in ranks[split:]]
-        weakest=weak_nodes[-1]
-        weakest_rank=unsorted_ranks[weakest]
+        weakest = weak_nodes[-1]
+        weakest_rank = unsorted_ranks[weakest]
         for n in weak_nodes:
             g.remove_node(n)
-        for n,r in ranks[0:split]:
-            if r<=weakest_rank:
+        for n, r in ranks[0:split]:
+            if r <= weakest_rank:
                 g.remove_node(n)
-        s_nodes = len([n for n in g.nodes if isinstance(n, int)])
-        w_nodes = g.number_of_nodes() - s_nodes
 
-        print('=> S_NODES:', s_nodes, 'W_NODES',w_nodes)
+        s_done = len(sids) <= sumsize
+        w_done = len(kwds) <= kwsize
 
-        if s_nodes <= sumsize: break
-        if w_nodes <= kwsize: break
+        if not s_done:
+            final_sids = sids
+        if not w_done:
+            final_kwds = kwds
 
-        if flip: g = g.reverse()
-    return gbak, ranks
+        if s_done and w_done: break
 
-
-short_text = """ 
-    I ate dinner. 
-    We had a three-course meal.
-    Brad came to dinner with us.
-    He loves fish tacos.
-    In the end, we all felt like we ate too much.
-    We all agreed; it was a magnificent evening.
-    """
+    return final_sids, final_kwds
 
 
-def process_text(text, ranker=nx.pagerank, sumsize=6, kwsize=6, trim=80, flip=True):
+def process_text(text, ranker=nx.pagerank, sumsize=6, kwsize=6, trim=80, show=False):
     lss = text2sents(text)
-    print("#SENT:",len(lss))
-    # print(lss)
+    print("#SENT:", len(lss))
+
     g = sents2graph(lss)
     print(g)
 
-    if g.number_of_edges() < 600:
+    if show and g.number_of_edges() < 600:
         nx.nx_agraph.write_dot(g, 'pic.gv')
 
     # for f,t in g.edges(): print(t,'<-',f)
-    g, ranks = textstar(g, ranker, sumsize, kwsize, trim, flip)
-    sids = [sid for (sid, _) in ranks if isinstance(sid,int)]
-    sids = sids[0:sumsize]
+    final_sids, final_kwds = textstar(g, ranker, sumsize, kwsize, trim)
+
+    sids = final_sids[0:sumsize]
     sids = sorted(sids)
-
-    print("SIDS:",sids)
+    print("SIDS:", sids)
     all_sents = [sent for (_, sent) in lss]
-    sents = [(i,all_sents[i]) for i in sids]
-    kwds = [w for (w,_) in ranks if isinstance(w, str)][0:kwsize]
+    sents = [(i, all_sents[i]) for i in sids]
 
-    if g.number_of_edges() < 600:
-       nx.nx_agraph.write_dot(g, 'final_pic.gv')
+    kwds = final_kwds[0:kwsize]
 
     return sents, kwds
 
 
-def test_textstar():
-    with open('../texts/english.txt', 'r') as f:
+def summarize(fname, ranker=nx.pagerank, show=False):
+    with open(fname + ".txt", 'r') as f:
         text = f.read()
     sents, kwds = process_text(
         text=text,
-        ranker=nx.betweenness_centrality
-    )
+        ranker=ranker,
+        show=show)
+    return sents, kwds
+
+
+def test_textstar():
+    sents, kwds = summarize('../texts/english', show=True)
+
     print('SUMMARY:')
     for sent in sents:
         print(*sent)
     print('\nKEYWORDS:')
-    print("; ".join(kwds)+".")
+    print("; ".join(kwds) + ".")
 
 
 if __name__ == "__main__":
